a34844d4cad428e1db699f1d114df34abfe7537a
Noctuurn <127550008+Noctuurn@users.noreply.github.com>
2024-10-02 20:07:05 +0200

    Avancée exercice 3

--- 01 - Programmation Orientée Objet/TP3_Mini_projet_POO_avec_attributs_privés.py
+++ 01 - Programmation Orientée Objet/TP3_Mini_projet_POO_avec_attributs_privés.py
@@ -1,1 +1,10 @@
-
+class Heure1:
+    def __init__(self,heureInit=0,minuteInit=0) :
+        self.__heure = heureInit
+        self.__minute = minuteInit
+    def getHeure(self):
+        return self.__heure
+    def getMinute(self):
+        return self.__minute
+    def setHeure(nouvelleHeure):
+        ...
