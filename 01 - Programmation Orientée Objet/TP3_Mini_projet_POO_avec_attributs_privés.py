c4876a73929ffa35774c050a69ff7236dba0c637
Noctuurn <127550008+Noctuurn@users.noreply.github.com>
2024-10-04 20:20:06 +0200

    Avancée TP3
    Avancée tp3 ( pas fini et pas fonctionnel )

--- 01 - Programmation Orientée Objet/TP3_Mini_projet_POO_avec_attributs_privés.py
+++ 01 - Programmation Orientée Objet/TP3_Mini_projet_POO_avec_attributs_privés.py
@@ -1,10 +1,28 @@
 class Heure1:
-    def __init__(self,heureInit=0,minuteInit=0) :
+    def __init__(self,heureInit=0,minuteInit=0) -> str :
         self.__heure = heureInit
         self.__minute = minuteInit
+
     def getHeure(self):
         return self.__heure
+    
     def getMinute(self):
         return self.__minute
-    def setHeure(nouvelleHeure):
-        ...
+    
+    def setHeure(self,nouvelleHeure):
+        set.__heure = nouvelleHeure
+
+    def setMinute(self,nouvelleMinute):
+        set.__minute = nouvelleMinute
+
+    def incrementer(self):
+        set.__minute = self.__minute + 1
+        if self.__minute > 60 :
+            set.__minute = 0
+            set.__heure = self.__heure + 1
+        if self.__heure > 23 :
+            set.__minute = 0
+            set.__heure = 0
+    def __str__(self):
+        return f"{self.__heure}:{self.__minute}"
+horloge = Heure1()
