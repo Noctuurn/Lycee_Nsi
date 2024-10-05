37497609d58666845da87c4e1a062520fd5f4031
Noctuurn <127550008+Noctuurn@users.noreply.github.com>
2024-10-05 19:07:21 +0200

    Fin du TP3 et création du fichier du tp4
    Fin du tp3 , crétion du fichier pour le tp4 mais aucune ligne écrite

--- 01 - Programmation Orientée Objet/TP3_Mini_projet_POO_avec_attributs_privés.py
+++ 01 - Programmation Orientée Objet/TP3_Mini_projet_POO_avec_attributs_privés.py
@@ -1,3 +1,4 @@
+# Exercice 1
 class Heure1:
     '''
     La clase Heure1 représente une horloge
@@ -6,13 +7,10 @@ class Heure1:
     Heure1(HeureInit(int),MinuteInit(int))
 
     Attributs de classe :
-    heureInit,
-    minuteInit
-
-    Attributs d'instance :
-    
     __heure,
     __minute,
+
+    Attributs d'instance :
     nouvelleHeure,
     nouvelleMinute
 
@@ -45,7 +43,7 @@ class Heure1:
         return f"{self.__heure}:{self.__minute}"
     
 # Exercice 2
-print("Pré-Incrémentation")
+print("Exercice 2 Pré-Incrémentation")
 h1 = Heure1(22,12)
 print(h1.getHeure()) 
 print(h1.getMinute())
@@ -57,7 +55,7 @@ print(h1)
 h1.incrementer()
 print(h1)
 
-
+# Exercice 3
 class Heure2:
     '''
     La clase Heure2 représente une horloge
@@ -66,39 +64,82 @@ class Heure2:
     Heure1(HeureInit(int),MinuteInit(int))
 
     Attributs de classe :
-    Aucun
+    __minute
 
     Attributs d'instance :
-    
-    __heure,
-    __minute,
     nouvelleHeure,
     nouvelleMinute
 
     '''
     def __init__(self,heureInit=0,minuteInit=0) -> str :
-        self.__heure = heureInit
-        self.__minute = minuteInit
+        self.__minute = minuteInit + heureInit*60
 
     def getHeure(self):
-        return self.__heure
+        return self.__minute // 60
     
     def getMinute(self):
-        return self.__minute
+        return self.__minute % 60
     
     def setHeure(self,nouvelleHeure):
-        self.__heure = nouvelleHeure
+        self.__minute = nouvelleHeure*60
+
 
     def setMinute(self,nouvelleMinute):
-        self.__minute = nouvelleMinute
+        self.__minute += nouvelleMinute
 
     def incrementer(self):
         self.__minute +=  1
-        if self.__minute >= 60 :
+
+        if self.__minute//60 >= 24 :
             self.__minute = 0
-            self.__heure += 1
-        if self.__heure >= 24 :
-            self.__minute = 0
-            self.__heure = 0
+
     def __str__(self):
-        return f"{self.__heure}:{self.__minute}"
+        return f"{self.__minute//60}:{self.__minute%60}"
+
+# Exercice 4
+
+print("Exercice 4 Pré-Incrémentation")
+h2 = Heure2(22,12)
+print(h2.getHeure()) 
+print(h2.getMinute())
+print(h2)
+print("Incrémentation et post-incrémentation")
+h2.setHeure(23)
+h2.setMinute(59)
+print(h2)
+h2.incrementer()
+print(h2)
+
+
+class Heure3:
+    '''
+    La clase Heure3 représente une horloge
+
+    Création d'une instance :
+    Heure3(hh(int),mm(int),ss(int))
+
+    Attributs de classe :
+    heure,
+    minute,
+    seconde,
+
+    Attributs d'instance :
+    h
+    '''
+    def __init__(self,hh=0,mm=0,ss=0) -> str :
+        self.heure = hh
+        self.minute = mm
+        self.seconde = ss
+
+    def afficheHeure(self):
+        return f"{self.heure}:{self.minute}:{self.seconde}"
+    
+    def setHeure(self,h):
+        self.heure = h
+
+# Exercice 6 
+h3 = Heure3()
+print("Exercice 6",h3.afficheHeure())
+h3.setHeure(13) , print(h3.afficheHeure())
+recreation = Heure3(9,50,18)
+print(recreation.afficheHeure())
