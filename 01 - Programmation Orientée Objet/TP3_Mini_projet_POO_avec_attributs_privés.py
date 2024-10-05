6f5a6e4d8ac395fd21fa0122c643d4e031f0ffc5
Noctuurn <127550008+Noctuurn@users.noreply.github.com>
2024-10-05 11:36:43 +0200

    Avancement sur le TP3
    Exo 1 et 2 fait, progression sur le 3)

--- 01 - Programmation Orientée Objet/TP3_Mini_projet_POO_avec_attributs_privés.py
+++ 01 - Programmation Orientée Objet/TP3_Mini_projet_POO_avec_attributs_privés.py
@@ -1,4 +1,22 @@
 class Heure1:
+    '''
+    La clase Heure1 représente une horloge
+
+    Création d'une instance :
+    Heure1(HeureInit(int),MinuteInit(int))
+
+    Attributs de classe :
+    heureInit,
+    minuteInit
+
+    Attributs d'instance :
+    
+    __heure,
+    __minute,
+    nouvelleHeure,
+    nouvelleMinute
+
+    '''
     def __init__(self,heureInit=0,minuteInit=0) -> str :
         self.__heure = heureInit
         self.__minute = minuteInit
@@ -10,19 +28,77 @@ class Heure1:
         return self.__minute
     
     def setHeure(self,nouvelleHeure):
-        set.__heure = nouvelleHeure
+        self.__heure = nouvelleHeure
 
     def setMinute(self,nouvelleMinute):
-        set.__minute = nouvelleMinute
+        self.__minute = nouvelleMinute
 
     def incrementer(self):
-        set.__minute = self.__minute + 1
-        if self.__minute > 60 :
-            set.__minute = 0
-            set.__heure = self.__heure + 1
-        if self.__heure > 23 :
-            set.__minute = 0
-            set.__heure = 0
+        self.__minute +=  1
+        if self.__minute >= 60 :
+            self.__minute = 0
+            self.__heure += 1
+        if self.__heure >= 24 :
+            self.__minute = 0
+            self.__heure = 0
     def __str__(self):
         return f"{self.__heure}:{self.__minute}"
-horloge = Heure1()
+    
+# Exercice 2
+print("Pré-Incrémentation")
+h1 = Heure1(22,12)
+print(h1.getHeure()) 
+print(h1.getMinute())
+print(h1)
+print("Incrémentation et post-incrémentation")
+h1.setHeure(23)
+h1.setMinute(59)
+print(h1)
+h1.incrementer()
+print(h1)
+
+
+class Heure2:
+    '''
+    La clase Heure2 représente une horloge
+
+    Création d'une instance :
+    Heure1(HeureInit(int),MinuteInit(int))
+
+    Attributs de classe :
+    Aucun
+
+    Attributs d'instance :
+    
+    __heure,
+    __minute,
+    nouvelleHeure,
+    nouvelleMinute
+
+    '''
+    def __init__(self,heureInit=0,minuteInit=0) -> str :
+        self.__heure = heureInit
+        self.__minute = minuteInit
+
+    def getHeure(self):
+        return self.__heure
+    
+    def getMinute(self):
+        return self.__minute
+    
+    def setHeure(self,nouvelleHeure):
+        self.__heure = nouvelleHeure
+
+    def setMinute(self,nouvelleMinute):
+        self.__minute = nouvelleMinute
+
+    def incrementer(self):
+        self.__minute +=  1
+        if self.__minute >= 60 :
+            self.__minute = 0
+            self.__heure += 1
+        if self.__heure >= 24 :
+            self.__minute = 0
+            self.__heure = 0
+    def __str__(self):
+        return f"{self.__heure}:{self.__minute}"
